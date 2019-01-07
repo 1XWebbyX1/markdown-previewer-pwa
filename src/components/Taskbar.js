3a3f0dfefa80520237ecab36d9a5538c6ca3babc
Khayati Asrani <devinc@TheDen-3.local>
2019-01-07 13:45:45 +0530

    update toolbar

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -6,18 +6,21 @@ class Taskbar extends React.Component{
   render(){
     return(
       <div id='taskbar'>
-       <i id="save" class='fa fa-save' onClick={this.props.save}></i>
-       <i id="info" onClick={this.props.toggleInfo} class='fa fa-info-circle'></i>
-       <i id="bold" onClick={this.props.onClick} className='fa fa-bold'></i>
-       <i id="italic" onClick={this.props.onClick} className='fa fa-italic'></i>
-       <i id="quote" onClick={this.props.onClick} className='fa fa-quote-left'></i>
-       <i id="link" onClick={this.props.onClick} className='fa fa-link'></i>
-       <i id="picture" onClick={this.props.onClick} className='fa fa-image'></i>
-       <i  onClick={this.props.onClick} className="fa fa-list"/>
-       <i  onClick={this.props.onClick} className="fa fa-list-ol"/>
-       <i  onClick={this.props.onClick} className="fa fa-code"/>
-       <i id="theme-switch" class='fa fa-toggle-on' onClick={this.props.switchTheme}></i>
+      <ul>
+       <li><i id="save" class='fa fa-save' onClick={this.props.save}></i></li>
+       <li><i id="info" onClick={this.props.toggleInfo} class='fa fa-info-circle'></i></li>
+       <li><i id="bold" onClick={this.props.onClick} className='fa fa-bold'></i></li>
+       <li><i id="italic" onClick={this.props.onClick} className='fa fa-italic'></i></li>
+       <li><i id="quote" onClick={this.props.onClick} className='fa fa-quote-left'></i></li>
+       <li><i id="link" onClick={this.props.onClick} className='fa fa-link'></i></li>
+       <li><i id="picture" onClick={this.props.onClick} className='fa fa-image'></i></li>
+       <li><i  onClick={this.props.onClick} className="fa fa-list"/></li>
+       <li><i  onClick={this.props.onClick} className="fa fa-list-ol"/></li>
+       <li><i  onClick={this.props.onClick} className="fa fa-code"/></li>
+       <li><i id="theme-switch" class='fa fa-toggle-on' onClick={this.props.switchTheme}></i></li>
+       </ul>
       </div>
+
     )
   }
 }
